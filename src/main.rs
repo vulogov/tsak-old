ca63f92778fd9cf199480695ec78e3357beef05b
Vladimir Ulogov <vladimir.ulogov@me.com>
2022-10-25 10:28:44 -0600

    Dev #25

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 
 use std;
-
 pub mod cmd;
 pub mod stdlib;
 pub mod lang;
 pub mod tsak_lib;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     cmd::init();
     std::process::exit(0);
 }
--- src/stdlib/system/system_metrics.rs
+++ src/stdlib/system/system_metrics.rs
@@ -1,36 +1,80 @@
-use lazy_static::lazy_static;
 extern crate log;
-use rhai::{Engine, Dynamic};
+use lazy_static::lazy_static;
+use rhai::{Engine, Map, Array, Dynamic, Identifier};
 use rhai::plugin::*;
+use std::{thread, time};
+use tokio::task;
 // Later add NetworkExt, NetworksExt, ProcessExt,
-use sysinfo::{System, SystemExt};
+use sysinfo::{System, SystemExt, CpuExt};
 use crate::stdlib::nr::metric::metric_type;
+use std::sync::Mutex;
 
 lazy_static! {
-    static ref METRIC_SYS: System = {
-        let mut sys = System::new_all();
-        sys.refresh_all();
+    static ref METRIC_SYS: Mutex<System> = {
+        let sys: Mutex<System> = Mutex::new(System::new_all());
+        sys.lock().unwrap().refresh_all();
         sys
     };
 }
 
+async fn update_sysinfo() {
+    log::debug!("Initiated sysinfo update thread");
+    loop {
+        thread::sleep(time::Duration::from_secs(5 as u64));
+        let mut sys = METRIC_SYS.lock().unwrap();
+        sys.refresh_all();
+    }
+}
+
 #[export_module]
 pub mod metrics_module {
+    pub fn refresh() {
+        log::debug!("Refrsheing sysinfo");
+        let mut sys = METRIC_SYS.lock().unwrap();
+        sys.refresh_all();
+        thread::sleep(time::Duration::from_millis(100));
+    }
     pub mod memory {
         pub fn total_memory() -> i64 {
-            METRIC_SYS.total_memory() as i64
+            METRIC_SYS.lock().unwrap().total_memory() as i64
         }
         pub fn used_memory() -> i64 {
-            METRIC_SYS.used_memory() as i64
+            METRIC_SYS.lock().unwrap().used_memory() as i64
         }
 
         pub mod m {
             pub fn total_memory() -> metric_type::Metric {
-                metric_type::Metric::init("total.memory".to_string(), Dynamic::from(METRIC_SYS.total_memory() as i64))
+                metric_type::Metric::init("total.memory".to_string(), Dynamic::from(METRIC_SYS.lock().unwrap().total_memory() as i64))
             }
             pub fn used_memory() -> metric_type::Metric {
-                metric_type::Metric::init("used.memory".to_string(), Dynamic::from(METRIC_SYS.used_memory() as i64))
+                metric_type::Metric::init("used.memory".to_string(), Dynamic::from(METRIC_SYS.lock().unwrap().used_memory() as i64))
             }
+        }
+    }
+
+    pub mod cpu {
+        pub fn cpus() -> Map {
+            let mut res = Map::new();
+            thread::sleep(time::Duration::from_millis(100));
+            METRIC_SYS.lock().unwrap().refresh_cpu();
+            for cpu in METRIC_SYS.lock().unwrap().cpus() {
+                let key = Identifier::from(cpu.name());
+                res.insert(key, Dynamic::from(cpu.cpu_usage() as f64));
+            }
+            res
+        }
+        pub fn usage() -> Array {
+            let mut res = Array::new();
+            thread::sleep(time::Duration::from_millis(100));
+            METRIC_SYS.lock().unwrap().refresh_cpu();
+            for cpu in METRIC_SYS.lock().unwrap().cpus() {
+                res.push(Dynamic::from(cpu.cpu_usage() as f64));
+            }
+            res
+        }
+
+        pub mod m {
+
         }
     }
 }
@@ -39,4 +83,5 @@ pub fn init(engine: &mut Engine) {
     log::trace!("Running STDLIB::System functions init");
     let module = exported_module!(metrics_module);
     engine.register_static_module("metrics", module.into());
+    let _ = task::spawn(update_sysinfo());
 }
