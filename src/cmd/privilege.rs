06f0927ec9cfb1a7f989a8062ffdb80a5d0a3a60
Vladimir Ulogov <vladimir.ulogov@me.com>
2023-03-03 09:48:50 -0700

    Dev #97

--- src/cmd/privilege.rs
+++ src/cmd/privilege.rs
@@ -22,21 +22,4 @@ pub fn check_privilege(c: cmd::Cli) {
             log::info!("You are running TSAK with SUID bit set");
         }
     }
-
-
-    if c.nr_account == "0" {
-        log::error!("You did not specified New Relic account");
-        std::process::exit(10)
-    }
-    log::debug!("NR Account is {}", c.nr_account);
-    if c.nr_api_key.is_empty() {
-        log::error!("You did not specified New Relic API key");
-        std::process::exit(10)
-    }
-    log::debug!("NR API key is {}", c.nr_api_key);
-    if c.nr_insert_key.is_empty() {
-        log::error!("You did not specified New Relic INSERT key");
-        std::process::exit(10)
-    }
-    log::debug!("NR INSERT key is {}", c.nr_insert_key);
 }
