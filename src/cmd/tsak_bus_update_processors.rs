31b1375719cd7a6e4a513c0dfa7f98f0254fa5a5
Vladimir Ulogov <vladimir.ulogov@me.com>
2023-03-05 00:11:30 -0700

    Dev #102 broken

--- src/cmd/tsak_bus_update_processors.rs
+++ src/cmd/tsak_bus_update_processors.rs
@@ -4,6 +4,10 @@ use std::{thread, time};
 use std::time::Duration;
 use scaproust::*;
 use voca_rs::*;
+use rhai::{Dynamic, Map};
+use crate::stdlib::bus::bus_update::try_update_bus_push;
+use crate::stdlib::timestamp::timestamp_module::timestamp_ms;
+use serde_json::{from_str};
 
 pub async fn bus_update_server_processor_main(c: cmd::Cli) -> () {
     log::debug!("update server reached");
@@ -11,17 +15,27 @@ pub async fn bus_update_server_processor_main(c: cmd::Cli) -> () {
         Ok(mut session) => {
             match session.create_socket::<Pull>() {
                 Ok(mut socket) => {
-                    let timeout = Duration::from_millis(5000);
-                    socket.set_recv_timeout(Some(timeout)).unwrap();
+                    // let timeout = Duration::from_millis(5000);
+                    // socket.set_recv_timeout(Some(timeout)).unwrap();
                     match socket.bind(&c.update_uri) {
                         Ok(_) => {
                             loop {
                                 match socket.recv() {
                                     Ok(data) => {
-                                        println!("{:?}", &data);
+                                        let buffer: String = std::str::from_utf8(&data).unwrap().to_string();
+                                        match from_str::<Map>(&buffer) {
+                                            Ok(pkt) => {
+                                                println!("{:?}", &pkt);
+                                            }
+                                            Err(err) => {
+                                                println!("{}", err);
+                                                continue;
+                                            }
+                                        }
                                     }
-                                    Err(_) => {
-                                        continue;
+                                    Err(err) => {
+                                        log::error!("error {}", err);
+                                        thread::sleep(time::Duration::from_millis(1000));
                                     }
                                 }
                             }
@@ -43,11 +57,22 @@ pub async fn bus_update_server_processor_main(c: cmd::Cli) -> () {
 }
 
 pub async fn bus_update_client_processor_main(c: cmd::Cli) -> () {
+    let mut upd_pkt = Map::new();
+    upd_pkt.insert("name".into(), Dynamic::from(c.name));
+    upd_pkt.insert("capability".into(), Dynamic::from(c.capability));
     loop {
+        upd_pkt.insert("timestamp".into(), Dynamic::from(timestamp_ms()));
         for s in c.update_server.split(",") {
             let srv = manipulate::trim(&manipulate::expand_tabs(&s.to_string(), 1), "");
             log::debug!("Will send heartbeat to {}", &srv);
+            match try_update_bus_push(srv, upd_pkt.clone()) {
+                Ok(_) => {},
+                Err(err) => {
+                    log::error!("update error: {}", err);
+                    break;
+                }
+            }
         }
-        thread::sleep(time::Duration::from_millis(5000));
+        thread::sleep(time::Duration::from_millis(15000));
     }
 }
--- src/stdlib/bus/bus_update.rs
+++ src/stdlib/bus/bus_update.rs
@@ -2,7 +2,7 @@ extern crate log;
 use scaproust::*;
 use std::time::Duration;
 use rhai::{Map, NativeCallContext, EvalAltResult};
-use serde_json::{to_string, from_str};
+use serde_json::{to_string};
 
 
 
@@ -15,20 +15,23 @@ pub fn try_update_bus_push(uri: String, d: Map) -> Result<bool, Box<EvalAltResul
         Ok(mut session) => {
             match session.create_socket::<Push>() {
                 Ok(mut socket) => {
-                    let timeout = Duration::from_millis(1000);
-                    socket.set_send_timeout(Some(timeout)).unwrap();
+                    //let timeout = Duration::from_millis(1000);
+                    //socket.set_send_timeout(Some(timeout)).unwrap();
                     match socket.connect(&uri) {
                         Ok(_) => {
                             match to_string(&d) {
                                 Ok(res) => {
-                                    match socket.send(res.as_bytes().to_vec()) {
-                                        Ok(_) => {},
-                                        Err(err) => {
-                                            let msg = format!("bus::cluster::update send error: {}", err);
-                                            log::error!("{}", &msg);
-                                            return Err(msg.into())
-                                        }
-                                    }
+                                    // match socket.send(res.as_bytes().to_vec()) {
+                                    //     Ok(_) => {
+                                    //         drop(session);
+                                    //         drop(socket);
+                                    //     },
+                                    //     Err(err) => {
+                                    //         let msg = format!("bus::cluster::update send error: {}", err);
+                                    //         log::error!("{}", &msg);
+                                    //         return Err(msg.into())
+                                    //     }
+                                    // }
                                 }
                                 Err(err) => {
                                     let msg = format!("bus::cluster::update converting to JSON: {}", err);
